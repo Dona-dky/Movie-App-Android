29cb22a68daef4b095981857c25f82a7afa06122
Dona <dona.denkey_dandjein@edu.devinci.fr>
2022-06-17 15:08:54 +0200

    link project to API

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bellano.themoviedblibrary.network.ApiHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ApiHelper.getTopRated()
     }
 }
